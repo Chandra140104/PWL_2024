f22bd6adc48e066e5e03cd6f14a063484bc98ffe
Chandra140104 <chandrabgs140104@gmail.com>
2024-08-29 22:30:23 +0700

    upload pertemuan 1

---
+++ resources/views/Pertemuan1/KTP.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .background {
+            text-align: center; 
+            margin-top: 250px;
+        }
+        img {
+            max-width: 100%; 
+           
+        }
+        .Header{
+            text-align: center;
+            font-size: 12px;
+            margin-top:-250px;
+        }
+        .Header h4{
+            margin:
+        }
+        .contain{
+            
+            text-align: center;
+            font-size: 10px;
+            margin-top:10px;
+            margin-left:-200px;
+        }
+        .contain p{
+            margin: 0;
+        }
+     
+        .nik {
+            display: inline-block;
+            width: 125px;
+            margin-left:70px;  
+        }
+       
+        .nama {
+            display: inline-block;
+            width: 130px;
+            margin-left:50px;
+        }
+        .ttl{
+            display: inline-block;
+            width: 85px;
+            margin-left:30px;
+        }
+        .kelamin{
+            display: inline-block;
+            width: 95px;
+            margin-left:80px;
+        }
+        .goldar{
+            display: inline-block;
+            width:75px;
+            margin-left:10px;
+        }
+        .Alamat{
+            display: inline-block;
+            width:130px;
+            margin-left:-20px;
+        }
+        .rt{
+            display: inline-block;
+            width:85px;
+            margin-left:-20px;
+        }
+        .kel{
+            display: inline-block;
+            width:80px;
+            margin-left:px;
+        }
+        .kec{
+            display: inline-block;
+            width:70px;
+            margin-left:20px;
+        }
+        .agama{
+            display: inline-block;
+            width:130px;
+            margin-left:-65px;
+        }
+        .status{
+            display: inline-block;
+            width:85px;
+            margin-left:-15px;
+        }
+        .pekerjaan{
+            display: inline-block;
+            width:120px;
+            margin-left:20px;
+        }
+        .kewarganegaraan{
+            display: inline-block;
+            width:90px;
+            margin-left:-35px;
+        }
+        .berlaku{
+            display: inline-block;
+            width:100px;
+            margin-left:10px;
+        }
+        .foto{
+            max-width:6%;
+            margin-left:700px;
+            margin-top:-150px;
+           
+          
+        }
+        .foto p{
+            font-size:9px;
+            margin-top:-1px;
+            margin-left:7px;
+        }
+        .tgl{
+            font-size:9px;
+            margin-left:13px;
+            margin-top:-10px;
+        }
+        .ttd{
+            max-width:3%;
+            margin-left:720px;
+            margin-top:;
+        }
+    </style>
+</head>
+<body>
+    <div class="background">
+        <img src="KTP 2.jpg">
+    </div>
+    <div class="Header">
+        <h4 style="margin:">PROVINSI JAWA TIMUR <br> KOTA KEDIRI</h4>
+    </div>
+    <div class="contain">
+        
+        <p style="font-size: 14px"><a class="nik">NIK</a> : 3 6 8 2 0 3 1 4 0 1 0 4 0 0 0 7</p>
+        <p><a class="nama">Nama</a> : CHANDRA BAGUS SULAKSONO</p>
+        <p><a class="ttl">Tempat/TglLahir</a> : KEDIRI, 14-01-2004</p>
+        <p><a class="kelamin">Jenis kelamin</a> : LAKI-LAKI <a class="goldar">Gol. Darah</a>: -</p>
+        <p><a class="Alamat">Alamat</a>: JL TOSAREN 1/51</p>
+        <p><a class="rt">RT/RW</a>: 004/002</p>
+        <p><a class="kel">Kel/Desa</a>: TOSAREN</p>
+        <p><a class="kec">Kecamatan</a>: PESANTREN</p>
+        <p><a class="agama">Agama</a>: ISLAM</p>
+        <p><a class="status">Status Perkawinan</a>: BELUM </p>
+        <p><a class="pekerjaan">Pekerjaan</a>: PELAJAR/MAHASISWA </p>
+        <p><a class="kewarganegaraan">Kewarganegaraan</a>: WNI </p>
+        <p><a class="berlaku">Berlaku Hingga</a>: SEUMUR HIDUP </p>
+    </div>
+    <div class="foto">
+        <img src="foto.jpeg">
+        <p>KOTA KEDIRI </p>
+        <div class="tgl">15-03-2021</div>
+
+    </div>
+    <div class="ttd">
+        <img src="ttd.png">
+    </div>
+</body>
+</html>
